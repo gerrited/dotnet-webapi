130b938680c10e01d6def443ad4ab1c229a7b39c
Gerrit <gerrit.edzards@gmail.com>
2021-12-19 16:44:43 +0100

    Add OpenTelemetry

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,6 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var oltpEndpoint = builder.Configuration.GetValue<string>("OltpEndpoint");
+
+builder.Services.AddOpenTelemetryTracing
+(
+    builder => 
+    {
+        builder.SetResourceBuilder
+        (
+            ResourceBuilder.CreateDefault().AddService("dotnet-webapi")
+        ).
+        AddAspNetCoreInstrumentation().
+        AddOtlpExporter
+        (
+            opt => { opt.Endpoint = new Uri(oltpEndpoint); }
+        );
+    }
+);
 
 var app = builder.Build();
 
